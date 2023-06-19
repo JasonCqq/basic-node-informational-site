2151e30878744ac79063b348ca85586d0d8fe447
Jason <121495300+jason21715@users.noreply.github.com>
2023-06-18 21:39:25 -0700

    Add error page on bad URL

--- index.js
+++ index.js
@@ -7,20 +7,56 @@ const http = require('http');
 const PORT = 8080;
 
 http.createServer((req, res) => {
-    
-    const myURL = new URL(req.url, 'http://localhost');
+    const myURL = new URL(req.url, "http://localhost");
     let filename = "." + myURL.pathname;
-    if(filename === './'){
+
+    if(filename === "./"){
         filename = "./index.html"
     }
 
-    fs.readFile(filename, 'utf-8', (err, data) => {
-        if (err){
-            res.writeHead(500);
-            return res.end();
+    fs.access(filename, fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.readFile('./404.html', 'utf-8', (error, errorPageData) => {
+                if (error){
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.writeHead(404, {'Content-Type' : 'text/html'});
+                    res.write(errorPageData);
+                    res.end();
+                }
+            });
+        } else {
+            fs.readFile(filename, 'utf-8', (error, data) => {
+                if (error){
+                    res.writeHead(500);
+                    res.end();
+                } else {
+                    res.writeHead(200, {'Content-Type' : 'text/html'});
+                    res.write(data);
+                    res.end();
+                }
+            })
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    })
+    });
+
+    // fs.readFile(filename, 'utf-8', (err, data) => {
+    //     if (err){
+    //         // res.writeHead(500);
+    //         // return res.end();
+    //         fs.readFile('./404.html', 'utf-8', (error, errorPageData) => {
+    //             if(error){
+    //                 res.writeHead(500);
+    //                 return res.end();
+    //             } else {
+    //                 res.writeHead(404, {'Content-Type': 'text/html'});
+    //                 res.write(errorPageData);
+    //                 res.end();
+    //             }
+    //         })
+    //     }
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     res.write(data);
+    //     res.end();
+    // })
 }).listen(PORT)
